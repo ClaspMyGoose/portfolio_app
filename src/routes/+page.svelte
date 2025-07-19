0485c54321f24b7c3781b48c8b920fdf06c75360
Michael Bearden <michael.bearden1@outlook.com>
2025-07-19 17:23:36 -0500

    beginning the soul crushing work of styling the sight to my aesthetic standards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-  <!-- Clean typography scale -->
+<!-- <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+
   <h1 class="text-3xl sm:text-4xl font-bold text-primary">Home /</h1>
   <p class="text-base sm:text-lg text-secondary leading-relaxed">What does this text look like?</p>
+</div> -->
+
+
+<div>
+    <h1>Home</h1>
 </div>
